7f9e54511154ba27ff8ee984ae844c5912108be8
bayuagung100 <bayuagung100@gmail.com>
2020-11-13 19:46:27 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { Component } from 'react';
+import { Button, Text, Container } from 'native-base';
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Button><Text>Button Native Base</Text></Button>
+      </Container>
+
+    )
+  }
+}
+
+export default App;
