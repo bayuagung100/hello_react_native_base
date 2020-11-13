5717f5560373cdf842625e2f8e66b2b7f2025d5c
bayuagung100 <bayuagung100@gmail.com>
2020-11-13 20:54:48 +0700

    beberapa component native base & fixing icon tidak muncul

--- App.js
+++ App.js
@@ -7,13 +7,102 @@
  */
 
 import React, { Component } from 'react';
-import { Button, Text, Container } from 'native-base';
+import { Image, ScrollView } from 'react-native';
+import { Header, Title, Button, Text, Container, Accordion, Content, Badge, Card, CardItem, Left, Thumbnail, Body, Icon, Right, Footer, FooterTab, } from 'native-base';
+
+const dataArray = [
+  { title: "First Element", content: "Lorem ipsum dolor sit amet" },
+  { title: "Second Element", content: "Lorem ipsum dolor sit amet" },
+  { title: "Third Element", content: "Lorem ipsum dolor sit amet" }
+];
 
 class App extends Component {
   render() {
     return (
       <Container>
-        <Button><Text>Button Native Base</Text></Button>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='arrow-back' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right>
+            <Button hasText transparent>
+              <Text>Cancel</Text>
+            </Button>
+          </Right>
+        </Header>
+        <ScrollView style={{marginTop:10}}>
+          <Button><Text>Button Native Base</Text></Button>
+          <Accordion
+            dataArray={dataArray}
+            headerStyle={{ backgroundColor: "#b7daf8" }}
+            contentStyle={{ backgroundColor: "#ddecf8" }}
+            icon="add"
+            expandedIcon="remove"
+            iconStyle={{ color: "green" }}
+            expandedIconStyle={{ color: "red" }}
+
+          />
+          <Badge style={{ backgroundColor: 'blue' }}>
+            <Text>2</Text>
+          </Badge>
+          <Content>
+            <Card>
+              <CardItem>
+                <Left>
+                  <Thumbnail source={{ uri: 'https://s3-ap-southeast-1.amazonaws.com/niomic/img-v1/c_login_logo.png' }} />
+                  <Body>
+                    <Text>NativeBase</Text>
+                    <Text note>GeekyAnts</Text>
+                  </Body>
+                </Left>
+              </CardItem>
+              <CardItem cardBody>
+                <Image source={{ uri: 'https://images.pexels.com/photos/2227826/pexels-photo-2227826.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' }} style={{ height: 200, width: null, flex: 1 }} />
+              </CardItem>
+              <CardItem>
+                <Left>
+                  <Button transparent>
+                    <Icon active name="thumbs-up" />
+                    <Text>12 Likes</Text>
+                  </Button>
+                </Left>
+                <Body>
+                  <Button transparent>
+                    <Icon active name="chatbubbles" />
+                    <Text>4 Comments</Text>
+                  </Button>
+                </Body>
+                <Right>
+                  <Text>11h ago</Text>
+                </Right>
+              </CardItem>
+            </Card>
+          </Content>
+        </ScrollView>
+        <Footer>
+          <FooterTab>
+            <Button>
+              <Icon name="apps" />
+            </Button>
+            <Button>
+              <Icon name="camera" />
+            </Button>
+            <Button active>
+              <Icon active name="navigate" />
+            </Button>
+            <Button>
+              <Icon name="person" />
+            </Button>
+            <Button style={{ backgroundColor: 'red' }}>
+              <Icon name="card" />
+            </Button>
+          </FooterTab>
+        </Footer>
       </Container>
 
     )
